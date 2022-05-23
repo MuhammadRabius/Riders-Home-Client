c6d0ebfe395517a163406b7bfc0fc644bd74c92f
MuhammadRabius <muhammadrabiussany@gmail.com>
2022-05-23 16:01:50 +0600

    media section added

--- src/components/Home/Media/Media.js
+++ src/components/Home/Media/Media.js
@@ -1,9 +1,43 @@
 import React from 'react';
+import tipsOne from '../../../assets/tipsBG (1).jpg'
+import tipsTwo from '../../../assets/tipsBG (2).jpg'
 
 const Media = () => {
       return (
-            <div>
-                  <h1>This is company media center</h1>
+            <div className='m-10 '>
+                  <h1 className='text-teal-400 font-bold text-center m-4'>TIPS ON THE MOTORCYCLE</h1>
+                  <div className='flex justify-center items-center gap-4 m-4 bg-stone-50'>
+                        <div class="hero min-h-screen">
+                        <div class="hero-overlay bg-opacity-100"></div>
+                        <div class="hero-content text-center text-neutral-content">
+                        <div class="max-w-md">
+                              <h1 class="mb-3 text-5xl font-bold">How To Maintain A Motorcycle Helmet? Do It Your Own</h1>
+                              <p class="mb-5">Storing a helmet in the proper way is very important. You can see we use the helmet for a certain period while we are ridging. Therefore, most of the time it kept stored for further usage. So, the storing should have to be in a clean and dry space.
+
+                              Don’t store the helmet under direct sunlight or in place of moisture or in humidity. It's better not to keep the helmet in closet or boxes because it does fungus buildup. Keeping the helmet in place of good airflow and comparatively in a warm place is healthful and hygienic.</p>
+                             
+                        </div>
+                        </div>
+                        </div>
+                        
+          <div class="hero min-h-screen ">
+                        <div class="hero-overlay bg-opacity-100"></div>
+                        <div class="hero-content text-center text-neutral-content">
+                        <div class="max-w-md">
+                              <h1 class="mb-3 text-5xl font-bold">How To Boost Motorcycle Performance</h1>
+                              <p class="mb-5">Always use good quality fuel from the reputed fuel station. Ensure using manufacturer recommended RON of fuel. It definitely increases motorcycle performance.
+                              Always use a clean and good manufacturer made spark plug. Periodically check the electrode gap and follow the manufacturer recommendation. While using aftermarket performance plugs, stay within manufacturer rating & standard.
+                              Keep the air filter element clean and dry. Clean the element after a regular interval and change it before wear. While using aftermarket air filter, do that within manufacturer recommendation.
+                               Use the manufacturer recommended grade and rating engine oil. Use reputed manufacturer engine oil and always continue with the same</p>
+                             
+                        </div>
+                        </div>
+                        </div>
+                  
+                  </div>
+                 <div class="card-actions justify-end">
+                  <button class="btn btn-primary">Read More</button>
+                  </div>
             </div>
       );
 };
