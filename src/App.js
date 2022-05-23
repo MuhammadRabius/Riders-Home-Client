23fcae98750bd70be2e1d19134214eaa90d5e4f2
MuhammadRabius <muhammadrabiussany@gmail.com>
2022-05-23 16:35:25 +0600

    footer added

--- src/App.js
+++ src/App.js
@@ -2,16 +2,20 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import NotFoundPage from './components/404Page/NotFoundPage';
+import Footer from './components/Footer/Footer';
+
 import Home from './components/Home/Home';
+import TopBanner from './components/Home/TopBanner/TopBanner';
 
 function App() {
   return (
     <div >
-     
+  
      <Routes>
        <Route path='/' element={<Home/>}> </Route>
        <Route path='*' element={<NotFoundPage/>}> </Route>
      </Routes>
+     <Footer/>
     </div>
   );
 }
--- src/components/Home/Media/Media.js
+++ src/components/Home/Media/Media.js
@@ -11,8 +11,8 @@ const Media = () => {
                         <div class="hero-overlay bg-opacity-100"></div>
                         <div class="hero-content text-center text-neutral-content">
                         <div class="max-w-md">
-                              <h1 class="mb-3 text-5xl font-bold">How To Maintain A Motorcycle Helmet? Do It Your Own</h1>
-                              <p class="mb-5">Storing a helmet in the proper way is very important. You can see we use the helmet for a certain period while we are ridging. Therefore, most of the time it kept stored for further usage. So, the storing should have to be in a clean and dry space.
+                              <h1 class="mb-3 text-5xl font-bold text-white">How To Maintain A Motorcycle Helmet? Do It Your Own</h1>
+                              <p class="mb-5 text-white">Storing a helmet in the proper way is very important. You can see we use the helmet for a certain period while we are ridging. Therefore, most of the time it kept stored for further usage. So, the storing should have to be in a clean and dry space.
 
                               Don’t store the helmet under direct sunlight or in place of moisture or in humidity. It's better not to keep the helmet in closet or boxes because it does fungus buildup. Keeping the helmet in place of good airflow and comparatively in a warm place is healthful and hygienic.</p>
                              
@@ -24,8 +24,8 @@ const Media = () => {
                         <div class="hero-overlay bg-opacity-100"></div>
                         <div class="hero-content text-center text-neutral-content">
                         <div class="max-w-md">
-                              <h1 class="mb-3 text-5xl font-bold">How To Boost Motorcycle Performance</h1>
-                              <p class="mb-5">Always use good quality fuel from the reputed fuel station. Ensure using manufacturer recommended RON of fuel. It definitely increases motorcycle performance.
+                              <h1 class="mb-3 text-5xl font-bold text-white">How To Boost Motorcycle Performance</h1>
+                              <p class="mb-5 text-white">Always use good quality fuel from the reputed fuel station. Ensure using manufacturer recommended RON of fuel. It definitely increases motorcycle performance.
                               Always use a clean and good manufacturer made spark plug. Periodically check the electrode gap and follow the manufacturer recommendation. While using aftermarket performance plugs, stay within manufacturer rating & standard.
                               Keep the air filter element clean and dry. Clean the element after a regular interval and change it before wear. While using aftermarket air filter, do that within manufacturer recommendation.
                                Use the manufacturer recommended grade and rating engine oil. Use reputed manufacturer engine oil and always continue with the same</p>
