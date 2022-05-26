a78970dd3339394bf3f4a6234a7faf353b53e6ea
MuhammadRabius <muhammadrabiussany@gmail.com>
2022-05-26 15:57:50 +0600

    add product for admin is created

--- src/components/UserDashboard/AddAProduct/AddProduct.js
+++ src/components/UserDashboard/AddAProduct/AddProduct.js
@@ -1,9 +1,50 @@
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useForm } from "react-hook-form";
+import { useNavigate} from 'react-router-dom';
+import { toast } from 'react-toastify';
+import auth from '../../../firebase.init';
 
 const AddProduct = () => {
+      const [user]=useAuthState(auth);
+      const navigate=useNavigate();
+      const { register, handleSubmit } = useForm();
+      const onSubmit = (data) => {
+            console.log(data)
+            const url = `http://localhost:5000/parts`;
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+        .then(res=> res.json())
+        .then(result =>{
+            console.log(result);
+        }, )
+        toast('Successfully Added')
+       navigate('/')
+      };
+
       return (
-            <div>
-                  <h1>This is Add Product</h1>
+             <div className='lg:flex justify-center items-center m-4 p-2 gap-3'>
+                  <div className='w-full lg:w-50 mx-auto p-6 mt-10 border-4'>
+                        <h3 className='text-center text-4xl font-bold'>Create New Product</h3>
+                   <form className='  flex flex-col  gap-2 mt-4' onSubmit={handleSubmit(onSubmit)}>
+                        
+                        <input type="email" name="email" value={user.email} id="email" className='border-2 p-2' {...register("email")} readOnly/>
+                        <input type="text" name="name" value={user.displayName} id="name" className='border-2 p-2' {...register("userName")} readOnly/>
+                        <input type="text" name="name2" placeholder='Please write product name' id="name2" className='border-2 p-2' {...register("name")} required />
+                        <input className='border-2 p-2' {...register("img")} placeholder='Category Photo URL' />
+                        <textarea className='border-2 p-2' {...register("description")}  placeholder='Write Medicine Description' />
+                        <input className='border-2 p-2' type="number" {...register("availableQuantity")}  placeholder='Please put available Quantity'/>
+                        <input className='border-2 p-2' type="number" {...register("minimumOrder")}  placeholder='Please put minimum order Quantity'/>
+                        <input className='border-2 p-2' type="number" {...register("price")}  placeholder='Per Unit Price'/>
+                        
+                        <input className=' p-2 bg-blue-500 text-white' type="submit" value='Add Product' />
+                  </form> 
+                  </div>
             </div>
       );
 };
--- src/components/UserDashboard/Dashboard.js
+++ src/components/UserDashboard/Dashboard.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link, Outlet } from 'react-router-dom';
 import auth from '../../firebase.init';
-
+import useAdmin from '../Hooks/useAdmin' 
 
 const Dashboard = () =>{
     const [user] = useAuthState(auth);
-    
+     const [admin] = useAdmin(user);
     return (
         <div class="drawer drawer-mobile">
             <input cla id="my-drawer-2" type="checkbox" class="drawer-toggle " />
@@ -20,10 +20,17 @@ const Dashboard = () =>{
                 <ul class="menu p-4 overflow-y-auto w-48 bg-base-100 bg-teal-50 text-base-content">
                    
                      <li><Link to="/dashboard">My Profile</Link></li>
+                     
+                     {
+                    user && <>
+                         
                      <li><Link to="/dashboard/myorder">My Order</Link></li>
                      <li><Link to="/dashboard/addreview">Add Review</Link></li>
+                         
+                         </>
+                     }
                               
-                    { user && <>
+                    { admin && <>
                         <li><Link to="/dashboard/users">All Users</Link></li>
                         <li><Link to="/dashboard/addproduct">Add a Product</Link></li>
                         <li><Link to="/dashboard/mp">Manage Products</Link></li>
